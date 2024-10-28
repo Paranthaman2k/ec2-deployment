d3f54e4e4c82b505a60ddc5a79aba60bc671520b
Paran <paranthaman2k@gmail.com>
2024-10-28 20:08:12 +0530

    deployment-file-update

---
+++ main.tf
@@ -0,0 +1,88 @@
+provider "aws" {
+  region = "us-east-1" # Replace with your preferred region
+}
+
+# VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Adjust based on your region
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "my_rta" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH access from any IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_project" {
+  ami                    = "ami-005fc0f236362e99f" # Replace with a valid AMI ID
+  instance_type          = "t2.medium"
+  subnet_id              = aws_subnet.my_subnet.id # Ensure this subnet ID is valid in your configuration
+  vpc_security_group_ids = [aws_security_group.my_sg.id] # Use security group IDs here
+
+  tags = {
+    Name = "MyProjectInstance"
+  }
+}
